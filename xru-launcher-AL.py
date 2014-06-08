c13f5a0e33c6854ba7efa7c01839aabc3960c84a
Wintermute0110 <wintermute0110@gmail.com>
2014-06-08 21:54:57 +0900

    Logger to console and file working OK. Software has been tested and all documented features work OK.

--- xru-launcher-AL.py
+++ xru-launcher-AL.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 # --- Global variables
 __software_version = '0.1.0';
 __config_configFileName = 'xru-launcher-AL-config.xml';
+__config_logFileName = 'xru-launcher-AL-log.txt';
 
 # --- Config file options global class (like a C struct)
 class ConfigFile:
@@ -18,14 +19,44 @@ class ConfigFileFilter:
 configuration = ConfigFile();
 
 # --- Program options (from command line)
-# __prog_option_dry_run = 0;
+__prog_option_verbose = 0;
+__prog_option_log = 0;
 
 # =============================================================================
-__debug_parse_File_Config = 0;
+class Log():
+  error = 1
+  warn = 2
+  info = 3
+  verb = 4
+  debug = 5
+
+# ---  Console print and logging
+f_log = 0;
+log_level = 3;
+def pprint(level, print_str):
+  global f_log;
+  global log_level;
+
+  # --- If file descriptor not open, open it
+  if f_log == 0:
+    f_log = open(__config_logFileName, 'w')
+    if __prog_option_verbose:
+      log_level = Log.verb;
+      
+  # --- Write to console depending on verbosity
+  if level <= log_level:
+    print print_str;
+
+  # --- Write to file
+  if level <= log_level:
+    if print_str[-1] != '\n':
+      print_str += '\n';
+    f_log.write(print_str) # python will convert \n to os.linesep
+
+# =============================================================================
 def parse_File_Config():
   "Parses configuration file"
-
-  print '[Parsing config file]';
+  pprint(Log.info, '[Parsing config file]');
   tree = ET.parse(__config_configFileName);
   root = tree.getroot();
 
@@ -43,72 +74,66 @@ def parse_File_Config():
       general_tag_found = 1;
       configFile.AL_config_file = root_child.text;
   if not general_tag_found:
-    print 'Configuration error. <configFile> tag not found';
+    pprint(Log.error, 'Configuration error. <configFile> tag not found');
     sys.exit(10);
 
   # --- Parse filters
   for root_child in root:
     if root_child.tag == 'launcher':
-      if __debug_parse_File_Config: 
-        print '<launcher>';
+      pprint(Log.debug, '<launcher>');
       if 'name' in root_child.attrib:
         filter_class = ConfigFileFilter();
         filter_class.name = root_child.attrib['name'];
-        if __debug_parse_File_Config: 
-          print ' name = ' + filter_class.name;
+        pprint(Log.debug, ' name = ' + filter_class.name);
         destDirFound = 0;
         for filter_child in root_child:
           if filter_child.tag == 'ROMsDest':
             if filter_child.text == None:
-              print '<ROMsDest> Empty directory name';
+              pprint(Log.error, '<ROMsDest> Empty directory name');
               sys.exit(10);
-            if __debug_parse_File_Config: 
-              print ' ROMsDest = ' + filter_child.text;
+            pprint(Log.debug, ' ROMsDest = ' + filter_child.text);
             destDirFound = 1;
             filter_class.destDir = filter_child.text
 
           elif filter_child.tag == 'FanartDest':
             if filter_child.text == None:
-              print '<FanartDest> Empty directory name';
-            if __debug_parse_File_Config: 
-              print ' FanartDest = ' + filter_child.text;
+              pprint(Log.info, '<FanartDest> Empty directory name');
+            pprint(Log.debug, ' FanartDest = ' + filter_child.text);
             filter_class.fanartDestDir = filter_child.text
 
           elif filter_child.tag == 'ThumbsDest':
             if filter_child.text == None:
-              print '<ThumbsDest> Empty directory name';
-            if __debug_parse_File_Config: 
-              print ' ThumbsDest = ' + filter_child.text;
+              pprint(Log.info, '<ThumbsDest> Empty directory name');
+            pprint(Log.debug, ' ThumbsDest = ' + filter_child.text);
             filter_class.thumbsDestDir = filter_child.text
             
           else:
-            print 'Unrecognised tag inside <launcher>';
+            pprint(Log.error, 'Unrecognised tag inside <launcher>');
             sys.exit(10);
 
         # Check for errors in this
         if not destDirFound:
-          print 'destination directory not found in config file';
+          pprint(Log.error, 'destination directory not found in config file');
           sys.exit(10);
 
         # Aggregate filter to configuration main variable
         configFile.launchers_dic[filter_class.name] = filter_class;
       else:
-        print '<launcher> tag does not have name attribute';
+        pprint(Log.error, '<launcher> tag does not have name attribute');
         sys.exit(10);
 
-  if __debug_parse_File_Config:
-    print 'AL_config_file = ', configFile.AL_config_file;
+  pprint(Log.debug, 'AL_config_file = ' + configFile.AL_config_file);
 
   # --- Check for errors
-
   return configFile;
 
 # =============================================================================
 def do_list():
   "Checks Advanced Launcher config file for updates"
 
-  print '[Listing Advanced Launcher launchers]';
+  pprint(Log.info, '[Listing Advanced Launcher launchers]');
   AL_configFileName = configuration.AL_config_file;
+  # Don't log "waiting bar like" massages
   print "Parsing Advanced Launcher configuration file...",;
   tree = ET.parse(AL_configFileName);
   print "done"
@@ -138,7 +163,7 @@ def do_list():
   #        <fanart>/path/007 - Everything or Nothing (USA, Europe) (En,Fr,De).jpg</fanart>
   #  ...
   for child in launchers:
-    print '<Launcher>';
+    pprint(Log.info, '<Launcher>');
 
     # --- Launcher data
     lauch_name = child.find('name');
@@ -149,13 +174,13 @@ def do_list():
     lauch_fanartpath = child.find('fanartpath');
     lauch_romext = child.find('romext');
 
-    print ' lauch_name        = "' + lauch_name.text + '"';
-    print ' lauch_application = ' + lauch_application.text;
-    print ' lauch_args        = ' + lauch_args.text;
-    print ' lauch_rompath     = ' + lauch_rompath.text;
-    print ' lauch_thumbpath   = ' + lauch_thumbpath.text;
-    print ' lauch_fanartpath  = ' + lauch_fanartpath.text;
-    print ' lauch_romext      = ' + lauch_romext.text;
+    pprint(Log.info, ' lauch_name        = "' + lauch_name.text + '"');
+    pprint(Log.info, ' lauch_application = ' + lauch_application.text);
+    pprint(Log.info, ' lauch_args        = ' + lauch_args.text);
+    pprint(Log.info, ' lauch_rompath     = ' + lauch_rompath.text);
+    pprint(Log.info, ' lauch_thumbpath   = ' + lauch_thumbpath.text);
+    pprint(Log.info, ' lauch_fanartpath  = ' + lauch_fanartpath.text);
+    pprint(Log.info, ' lauch_romext      = ' + lauch_romext.text);
 
     # --- Traverse the list of roms
     continue;
@@ -167,28 +192,27 @@ def do_list():
       rom_fanart = child_roms.find('fanart');
 
       if rom_name.text is not None:
-        print ' rom_name     = ' + rom_name.text;
+        pprint(Log.info, ' rom_name     = ' + rom_name.text);
       if rom_filename.text is not None:
-        print ' rom_filename = ' + rom_filename.text;
+        pprint(Log.info, ' rom_filename = ' + rom_filename.text);
       if rom_thumb.text is not None:
-        print ' rom_thumb    = ' + rom_thumb.text;
+        pprint(Log.info, ' rom_thumb    = ' + rom_thumb.text);
       if rom_fanart.text is not None:
-        print ' rom_fanart   = ' + rom_fanart.text;
+        pprint(Log.info, ' rom_fanart   = ' + rom_fanart.text);
 
 def do_list_config():
   "List of this program configuration file"
-  print '[Listing configuration]';
-  
-  print 'Advanced Launcher configuration file';
-  print ' ' + configuration.AL_config_file;
+  pprint(Log.info, '[Listing configuration]');  
+  pprint(Log.info, 'Advanced Launcher configuration file');
+  pprint(Log.info, ' ' + configuration.AL_config_file);
 
   for key in configuration.launchers_dic:
     launcher = configuration.launchers_dic[key];
-    print '<Launcher>';
-    print ' name = ' + launcher.name;
-    print ' destDir = ' + launcher.destDir;
-    print ' fanartDestDir = ' + launcher.fanartDestDir;
-    print ' thumbsDestDir = ' + launcher.thumbsDestDir;
+    pprint(Log.info, '<Launcher>');
+    pprint(Log.info, ' name = ' + launcher.name);
+    pprint(Log.info, ' destDir = ' + launcher.destDir);
+    pprint(Log.info, ' fanartDestDir = ' + launcher.fanartDestDir);
+    pprint(Log.info, ' thumbsDestDir = ' + launcher.thumbsDestDir);
     
 #
 # Checks AL configuration file. The following checks are performed
@@ -198,7 +222,7 @@ def do_list_config():
 def do_check():
   "Checks Advanced Launcher config file for updates"
 
-  print '[Listing Advanced Launcher launchers]';
+  pprint(Log.info, '[Checking Advanced Launcher launchers]');
   AL_configFileName = configuration.AL_config_file;
   print "Parsing Advanced Launcher configuration file...",;
   tree = ET.parse(AL_configFileName);
@@ -209,16 +233,16 @@ def do_check():
   update_launchers_list = [];
   not_checked_launchers_list = [];
   for child in launchers:
-    print '<Launcher>';
+    pprint(Log.info, '<Launcher>');
     
     # --- Launcher data
     lauch_name = child.find('name');
     lauch_application = child.find('application');
     lauch_rompath = child.find('rompath');
 
-    print ' lauch_name = "' + lauch_name.text + '"';
-    # print ' lauch_application = ' + lauch_application.text;
-    # print ' lauch_rompath     = ' + lauch_rompath.text;
+    pprint(Log.info, ' lauch_name = "' + lauch_name.text + '"');
+    pprint(Log.verb, ' lauch_application = ' + lauch_application.text);
+    pprint(Log.verb, ' lauch_rompath     = ' + lauch_rompath.text);
 
     # --- Only do Genesis to DEBUG
     # if lauch_name.text != 'Genesis (Mednafen)':
@@ -234,24 +258,24 @@ def do_check():
     localPath = AL_rom_path;
     launcher_name = lauch_name.text;
     if not os.path.isdir(localPath):
-      print ' Not found AL configuration file ROM path'
+      pprint(Log.info,  ' Not found AL configuration file ROM path')
       # Locate laucher in config file
       if launcher_name in configuration.launchers_dic:
-        print ' Using configured ROM local path for this launcher'
+        pprint(Log.info,  ' Using configured ROM local path for this launcher')
         launcher_conf = configuration.launchers_dic[launcher_name];
         localPath = launcher_conf.destDir;
         if not os.path.isdir(localPath):
-          print ' Local ROM path configuration not found';
-          print ' Aborting checking this launcher';
+          pprint(Log.info,  ' Local ROM path configuration not found');
+          pprint(Log.info,  ' Aborting checking this launcher');
           not_checked_launchers_list.append(launcher_name);
           continue;
       else:
-        print ' Local ROM path configuration for this launcher not found'; 
-        print ' Aborting checking this launcher';
+        pprint(Log.info,  ' Local ROM path configuration for this launcher not found');
+        pprint(Log.info,  ' Aborting checking this launcher');
         not_checked_launchers_list.append(launcher_name);
         continue;
     else:
-      print ' Using AL configuration file ROM path';
+      pprint(Log.info,  ' Using AL configuration file ROM path');
 
     # - Traverse the list of roms and check
     launcher_ROM_list = [];
@@ -279,27 +303,27 @@ def do_check():
         if tail not in launcher_ROM_list:
           error_AL_missing_rom += 1;
 
-    print ' ' + str(error_AL_not_found_rom) + ' not found ROMs in AL configuration file';
-    print ' ' + str(error_AL_missing_rom) + ' missing ROMs in AL configuration file';
+    pprint(Log.info,  ' ' + str(error_AL_not_found_rom) + ' not found ROMs in AL configuration file');
+    pprint(Log.info,  ' ' + str(error_AL_missing_rom) + ' missing ROMs in AL configuration file');
 
     if error_AL_not_found_rom or error_AL_missing_rom:
       update_launchers_list.append(launcher_name);
 
   # --- Report if AL needs update or not
-  print '[Report]';
+  pprint(Log.info,  '[Report]');
   if len(not_checked_launchers_list) > 0:
-    print 'The following launchers were not checked';
+    pprint(Log.info,  'The following launchers were not checked');
     for launcher in not_checked_launchers_list:
-      print ' ' + launcher;
+      pprint(Log.info,  ' ' + launcher);
   else:
-    print 'All launchers were checked.';
+    pprint(Log.info,  'All launchers were checked.');
 
   if len(update_launchers_list) > 0:
-    print 'Advanced Launcher needs an update for the following launchers';
+    pprint(Log.info,  'Advanced Launcher needs an update for the following launchers');
     for launcher in update_launchers_list:
-      print ' ' + launcher;
+      pprint(Log.info,  ' ' + launcher);
   else:
-    print 'All launchers up to date';
+    pprint(Log.info,  'All launchers up to date');
 
 def do_printHelp():
   print """
@@ -321,7 +345,13 @@ def do_printHelp():
   
 \033[32mOptions:
   \033[35m-h\033[0m, \033[35m--help\033[0m
-    Print short command reference"""
+    Print short command reference
+    
+  \033[35m-v\033[0m, \033[35m--verbose\033[0m
+    Print more information about what's going on
+
+  \033[35m-l\033[0m, \033[35m--log\033[0m
+    Save program output in xru-launcher-AL-log.txt"""
 
 # =============================================================================
 def main(argv):
@@ -330,11 +360,18 @@ def main(argv):
 
   # - Command line parser
   parser = argparse.ArgumentParser()
+  parser.add_argument("--verbose", help="print version", action="store_true")
+  parser.add_argument("--log", help="print version", action="store_true")
   parser.add_argument("command", help="usage, list, list-config, check")
   args = parser.parse_args();
   
   # --- Optional arguments
+  global __prog_option_verbose;
+  global __prog_option_log;
 
+  if args.verbose: __prog_option_verbose = 1;
+  if args.log:     __prog_option_log = 1;
+  
   # --- Positional arguments that don't require parsing of the config file
   if args.command == 'usage':
     do_printHelp();
@@ -355,7 +392,7 @@ def main(argv):
     do_check();
     
   else:
-    print 'Unrecognised command';
+    pprint(Log.error, 'Unrecognised command');
 
   sys.exit(0);
 
